758d6b3f89b7d26d7a144233a5bac7b049040c89
somber-robot <somberrobot@gmail.com>
2025-06-16 23:16:10 +0100

    Get player input

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function getCpuChoice(){
     const options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random() * 2.9)];
 }
+
+function getHumanChoice(){
+    let choice = prompt("rock, paper or scissors?", "");
+    return choice;
+}
+

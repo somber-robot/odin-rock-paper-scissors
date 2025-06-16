16a04a0c0871241777a7c0862307698ca76d0923
somber-robot <somberrobot@gmail.com>
2025-06-16 23:26:12 +0100

    Add single round functionality

--- script.js
+++ script.js
@@ -8,3 +8,47 @@ function getHumanChoice(){
     return choice;
 }
 
+let humanScore = 0, cpuScore = 0;
+
+function playRound(humanChoice, cpuChoice){
+    humanChoice = humanChoice.toLowerCase();
+
+    console.log(`Cpu: ${cpuChoice}`);
+
+    if (humanChoice === "rock"){
+        if (cpuChoice === "paper"){
+            console.log("You lose! Paper beats Rock");
+            cpuScore++;
+        }else if (cpuChoice === "rock"){
+            console.log("You draw!");
+        }else{
+            console.log("You win! Rock beats Scissors");
+            humanScore++;
+        }
+    }else if (humanChoice === "paper"){
+        if (cpuChoice === "paper"){
+            console.log("You draw!");
+        }else if (cpuChoice === "rock"){
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+        }else{
+            console.log("You lose! Scissors beats Paper");
+            cpuScore++;
+        }
+    }else{
+        if (cpuChoice === "paper"){
+            console.log("You win! Scissors beats Paper");
+            humanScore++;
+        }else if (cpuChoice === "rock"){
+            console.log("You lose! Rock beats Scissors");
+            cpuScore++;
+        }else{
+            console.log("You draw!");
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const cpuSelection = getCpuChoice();
+
+playRound(humanSelection, cpuSelection);

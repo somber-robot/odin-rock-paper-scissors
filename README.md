07469a3e6c808de12a6633cef5a20a4c9bedb044
somber-robot <somberrobot@gmail.com>
2025-06-16 23:11:21 +0100

    Select CPU choice

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-rock-paper-scissors
+
+This is a basic Rock-Paper-Scissors console app created as part of the Odin Project Foundations course.
---
+++ script.js
@@ -0,0 +1,4 @@
+function getCpuChoice(){
+    const options = ["rock", "paper", "scissors"];
+    return options[Math.floor(Math.random() * 2.9)];
+}
